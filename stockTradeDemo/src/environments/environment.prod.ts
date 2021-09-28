d5cb8a55e9f15ec3d3dab78e76e9cb4929409f31
SRYVPRASAD <sryvprasad@gmail.com>
2021-09-28 13:03:10 +1000

    login and register done
    connection to firebase and auth

---
+++ stockTradeDemo/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+ firebaseConfig : {
+    apiKey: "AIzaSyAZibASqP9iMQjMpT8xJeH3ZVKvriZyfeg",
+    authDomain: "stock-trade-demo.firebaseapp.com",
+    databaseURL: "https://stock-trade-demo-default-rtdb.firebaseio.com",
+    projectId: "stock-trade-demo",
+    storageBucket: "stock-trade-demo.appspot.com",
+    messagingSenderId: "768002186308",
+    appId: "1:768002186308:web:b8b491fb34008199b27011",
+    measurementId: "G-3JYVCHEPDK"
+  },
+};
